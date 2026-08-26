3c269a0879eaa37e32147afd3cb1276037f84f0d
subhammohanty-sys <subhammohantygithub@gmail.com>
2026-08-27 03:15:43 +0530

    fixes

--- src/models/blacklist.model.js
+++ src/models/blacklist.model.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
+
 const tokenBlacklistSchema = new mongoose.Schema({
     token: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "Token is required"],
+        unique: [true, "Token is already blacklisted"]
     }
-}, { timestamps: true });
+}, {
 
-module.exports = mongoose.model("TokenBlacklist", tokenBlacklistSchema);
+    timestamps: true
+})
+
+tokenBlacklistSchema.index({ createdAt: 1 }, {
+    expireAfterSeconds: 60 * 60 * 24 * 7 //delete token after 7 days
+});
+
+
+const TokenBlackListModel = mongoose.model("TokenBlacklist", tokenBlacklistSchema);
+module.exports = TokenBlackListModel
