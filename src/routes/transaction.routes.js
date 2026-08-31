75d846f3891a8a13b97f0fb5439f6b0b76d15a66
subhammohanty-sys <subhammohantygithub@gmail.com>
2026-08-31 22:25:27 +0530

    fixes

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const authMiddleware = require('../middleware/auth.middleware');
 const transactionController = require("../controllers/transaction.controller")
+const idempotencyMiddleware = require("../middleware/idempotency.middleware")
 
 const transactionRoutes = Router();
 
@@ -9,13 +10,13 @@ const transactionRoutes = Router();
  * - Create a new transaction
  */
 
-transactionRoutes.post("/", authMiddleware.authMiddleware, transactionController.createTransaction)
+transactionRoutes.post("/", authMiddleware.authMiddleware, idempotencyMiddleware.verifyIdempotencyKey, transactionController.createTransaction)
 
 
 /**
  * - POST /api/transactions/system/initial-funds
  * - Create initial funds transaction from system user
  */
-transactionRoutes.post("/system/initial-funds", authMiddleware.authSystemUserMiddleware, transactionController.createInitialFundsTransaction)
+transactionRoutes.post("/system/initial-funds", authMiddleware.authSystemUserMiddleware, idempotencyMiddleware.verifyIdempotencyKey, transactionController.createInitialFundsTransaction)
 
 module.exports = transactionRoutes;
